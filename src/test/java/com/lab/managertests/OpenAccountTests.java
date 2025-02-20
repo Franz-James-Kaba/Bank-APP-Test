6703a1bffd601fcfc3b745027bcb4232f61ddbd6
Franz-James-Kaba <franz-james.kaba@amalitech.com>
2025-02-20 10:33:51 +0000

    This is the commit of the open account negative tests

--- src/test/java/com/lab/managertests/OpenAccountTests.java
+++ src/test/java/com/lab/managertests/OpenAccountTests.java
@@ -2,6 +2,9 @@ package com.lab.managertests;
 
 import com.lab.base.Setup;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class OpenAccountTests extends Setup {
     @Test
@@ -11,6 +14,18 @@ public class OpenAccountTests extends Setup {
         openAccount.selectCustomerDropDwn("Harry Potter");
         openAccount.setCurrencyDropDown("Dollar");
         openAccount.clickProcessButton();
+    }
+
+    @Test
+    public void testfailOpenAccount(){
+        homePage.clickManagerLogin();
+        managerDasboard.clickOpenAccountButton();
+        // Verify that trying to select an invalid customer name throws an exception
+        assertThrows(NoSuchElementException.class, () -> {
+            openAccount.selectCustomerDropDwn("Hunter Parker");
+            openAccount.setCurrencyDropDown("Dollar");
+            openAccount.clickProcessButton();
+        });
     }
 
 
